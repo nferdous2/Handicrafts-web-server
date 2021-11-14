abde47bb52c08173c39cc3104a45797ccf4d57eb
nferdous2 <noorjahanferdous029@gmail.com>
2021-11-14 20:08:15 +0600

    add

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 require('dotenv').config();
@@ -12,7 +11,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('Running Tour server')
+    res.send('Running Decore And craft server')
 });
 
 async function run() {
@@ -20,20 +19,54 @@ async function run() {
         await client.connect();
         const database = client.db("buy_sell");
         const productsCollection = database.collection("products");
-
+        const usersCollection = database.collection('users');
+        const addReviewCollection = database.collection('review')
         // get api
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
         });
-        // post api
-        app.post('/products/user', async (req, res) => {
-            const product = req.body;
-            console.log('post api', product);
-            const result = await productsCollection.insertOne(product);
+        //post for add register users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+        //making admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+        //add review
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await addReviewCollection.insertOne(review);
             console.log(result);
             res.json(result)
+
+        });
+        //orders handle
+        app.post('/myOrders', async (req, res) => {
+            const orders = req.body;
+            const result = await addReviewCollection.insertOne(orders);
+            console.log(result);
+            res.json(result)
+
         });
         //delete api
         app.delete('/products/:id', async (req, res) => {
